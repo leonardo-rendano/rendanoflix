3e84b8645c95fe0de3a699593005d1584fe2c340
Leonardo-Rendano <62267208+Leonardo-Rendano@users.noreply.github.com>
2020-07-28 20:11:07 -0300

    Criação do formulário da página de cadastro

--- src/components/pages/cadastro/Categoria/Index.js
+++ src/components/pages/cadastro/Categoria/Index.js
@@ -7,8 +7,18 @@ function cadastroCategoria(props) {
     <PageDefault >
       <h1>Cadastro de Categoria</h1>
 
-      
+      <form>
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
 
+        <button>
+          Cadastrar
+        </button>
+      </form>
 
       <Link to="/" >
         Ir para Home
